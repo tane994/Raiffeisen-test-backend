3c2a964ebc343363427e220870dfcd0ebb7e086e
Andrés Tanesini <andres.tanesini94@gmail.com>
2024-10-17 10:53:34 +0200

    feat: create entity for note

---
+++ src/main/java/com/example/raiffeisen_backend/note/Note.java
@@ -0,0 +1,62 @@
+package com.example.raiffeisen_backend.note;
+
+import jakarta.persistence.Entity;
+
+import java.time.LocalDate;
+
+@Entity
+public class Note {
+    private String title;
+    private String content;
+    private LocalDate createdAt;
+    private LocalDate updateAt;
+
+    public Note(String title, String content, LocalDate createdAt, LocalDate updateAt) {
+        this.title = title;
+        this.content = content;
+        this.createdAt = LocalDate.now();
+        this.updateAt = LocalDate.now();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDate getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(LocalDate updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Note{" +
+                "title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", createdAt=" + createdAt +
+                ", updateAt=" + updateAt +
+                '}';
+    }
+}

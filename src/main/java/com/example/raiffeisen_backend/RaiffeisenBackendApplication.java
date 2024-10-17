a227b8b936290227a8db157a4c996ddba25e7c0a
Andrés Tanesini <andres.tanesini94@gmail.com>
2024-10-17 09:51:34 +0200

    Initial commit

---
+++ src/main/java/com/example/raiffeisen_backend/RaiffeisenBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.raiffeisen_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaiffeisenBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaiffeisenBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/raiffeisen_backend/RaiffeisenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.raiffeisen_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaiffeisenBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
